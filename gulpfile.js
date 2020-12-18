2afa7bd5ff38023d6ae301f36e345be34f59de8b
Margarita Chirillova <margarita.chirillova@bshg.com>
2020-12-18 19:30:29 +0100

    add package json

--- gulpfile.js
+++ gulpfile.js
@@ -3,27 +3,27 @@
 'use strict';
 
 // dependencies
-var gulp = require('gulp');
-var markdownpdf = require('gulp-markdown-pdf');
-var rename = require("gulp-rename");
-var NwBuilder = require('node-webkit-builder');
+const gulp = require('gulp');
+const markdownpdf = require('gulp-markdown-pdf');
+const rename = require("gulp-rename");
+const NwBuilder = require('node-webkit-builder');
 
 // node.js modules
-var path = require('path');
-var del = require('del');
-var cjson = require('cjson');
+const path = require('path');
+const del = require('del');
+const cjson = require('cjson');
 
 // show simple help menu
 require('gulp-help')(gulp);
 
-var paths = {
+let paths = {
     scripts: [path.join(__dirname, 'app', 'scripts', 'core', '**', '*js')]
 };
 
 // -----------------------------------------------------------------------------------------------------------------
 
 gulp.task('build', 'Building application for distribution.', function (cb) {
-    var nw = new NwBuilder({
+    let nw = new NwBuilder({
         files: [
             'app/**/**',
             'package.json'
